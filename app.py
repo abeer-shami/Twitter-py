b2ef11d1d4797e90dbe5b8bdb6b13ee057513023
abeer-shami <abeer.shami887@gmail.com>
2017-11-14 15:56:29 +0200

    editing the routing

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 
-from flask import Flask
+from flask import Flask,render_template
 app=Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return render_template('home.html')
+
+
+@app.route('/Aboutus')
+def Aboutus():
+    return render_template('Aboutus.html')  
 
 if __name__ == '__main__':
     app.run(debug=True)
